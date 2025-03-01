e8d089c9f592572c1feacf800904c4183c2fcbb0
r-happpy <hr325913@gmail.com>
2025-03-01 13:21:27 +0900

    add param

--- src/router.go
+++ src/router.go
@@ -1,37 +1,113 @@
 package main
 
 import (
-	"back/handler"
+    "back/handler"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+    "github.com/labstack/echo"
+    "github.com/labstack/echo/middleware"
 )
 
 func newRouter() *echo.Echo {
-	e := echo.New()
+    e := echo.New()
 
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.Use(middleware.CORS())
-	e.POST("/user", handler.CreateUser)
-	e.POST("/login", handler.Login)
-	e.GET("/", func (c echo.Context) error {
-		return c.String(200, "Hello, World!!")
-	})
+    e.Use(middleware.Logger())
+    e.Use(middleware.Recover())
+    e.Use(middleware.CORS())
+    e.POST("/user", handler.CreateUser) // Body: { "username": "string", "password": "string", "email": "string" }
+    e.POST("/login", handler.Login) // Body: { "email": "string", "password": "string" }
+    e.GET("/", func(c echo.Context) error {
+        return c.String(200, "Hello, World!!")
+    })
 
-	api := e.Group("/api")
-	api.Use(middleware.JWTWithConfig(handler.Config))
-	api.POST("/space", handler.AddSpace)
-	api.GET("/space/:id", handler.GetSpace)
-	api.GET("/space/:id/member", handler.GetMembers)
-	api.POST("/space/:id/member", handler.AddMembers)
-	api.GET("/spaces", handler.GetSpaces)
-	api.GET("/space/:id/children", handler.GetChildrens)
-	api.GET("/space/:id/good", handler.GetGoods)
-	api.GET("/space/:sid/good/:gid", handler.GetGood)
-	api.POST("/space/:sid/good/:gid", handler.ToggleGood)
-	api.POST("/space/:id/good", handler.AddGoods)
-	api.GET("/profile", handler.GetProfile)
+    // JWT Middleware
+    api := e.Group("/api")
+    api.Use(middleware.JWTWithConfig(handler.Config))
+    api.POST("/space", handler.AddSpace) // Body: { "space_name": "string", "parent_id": "string" }
 
-	return e
+    /*
+        /space/:id
+        スペースの詳細を取得
+        Parameters:
+            id: string
+    */
+    api.GET("/space/:id", handler.GetSpace)
+
+    /*
+        /space/:id/member
+        スペースのメンバーを取得
+        Parameters:
+            id: string
+    */
+    api.GET("/space/:id/member", handler.GetMembers)
+
+    /*
+        /space/:id/member
+        スペースのメンバーを追加
+        Parameters:
+            id: string
+        Body: { "email": "string", "name": "string", "admin": "bool" }
+    */
+    api.POST("/space/:id/member", handler.AddMembers)
+
+    /*
+        /spaces
+        全てのスペースを取得
+        Parameters:
+            なし
+    */
+    api.GET("/spaces", handler.GetSpaces)
+
+    /*
+        /space/:id/children
+        スペースの子スペースを取得
+        Parameters:
+            id: string
+    */
+    api.GET("/space/:id/children", handler.GetChildrens)
+
+    /*
+        /space/:id/good
+        スペースのグッズを取得
+        Parameters:
+            id: string
+    */
+    api.GET("/space/:id/good", handler.GetGoods)
+
+    /*
+        /space/:sid/good/:gid
+        特定のグッズを取得
+        Parameters:
+            sid: string
+            gid: string
+    */
+    api.GET("/space/:sid/good/:gid", handler.GetGood)
+
+    /*
+        /space/:sid/good/:gid
+        グッズの状態をトグル
+        Parameters:
+            sid: string
+            gid: string
+        Body: { "email": "string" }
+    */
+    api.POST("/space/:sid/good/:gid", handler.ToggleGood)
+
+    /*
+        /space/:id/good
+        スペースにグッズを追加
+        Parameters:
+            id: string
+        Body: { "good_name": "string", "description": "string" }
+    */
+    api.POST("/space/:id/good", handler.AddGoods)
+
+    /*
+        /profile
+        ユーザープロフィールを取得
+        Parameters:
+            なし
+    */
+    api.GET("/profile", handler.GetProfile)
+
+    return e
 }
